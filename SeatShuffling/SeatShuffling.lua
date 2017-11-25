08ade5f8e1416d8e0d10724def897951a3de381c
TheOfficialSouthern <33977998+TheOfficialSouthern@users.noreply.github.com>
2017-11-25 04:22:26 -0500

    Add files via upload

---
+++ SeatShuffling/SeatShuffling.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function() 
+	while true do 
+		Citizen.Wait(0) 
+		  if IsPedInAnyVehicle(GetPlayerPed(-1), false) 
+			 then if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) == GetPlayerPed(-1) 
+			 then if GetIsTaskActive(GetPlayerPed(-1), 165) 
+			 then SetPedIntoVehicle(GetPlayerPed(-1), GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) SetPedConfigFlag(lPed, 32, true) 
+		  	 end 
+		 end 
+	 end 
+ end 
+end)
